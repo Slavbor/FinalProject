9db2eb5bbe50944e21dd48d47e60ee5bea6c864b
Slavbor <shipmate@mail.ru>
2023-02-13 20:54:18 +0300

    Project completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+WriteLine();
+WriteLine("    # ArrAy WiTh ElEmEnTs SiZe <=3 GeNeRaTiNg & PriNTiNg #");
+WriteLine();
+
+Write("Enter any strings separated by space: ");
+string[] array = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)!;
+
+
+PrintArray(LessEqual3Array(array));
+
+
+
+
+
+
+
+
+
+
+
+string[] LessEqual3Array(string[] ArrayMethod)
+{
+    int summ = 0;
+    foreach (var element in ArrayMethod)
+    {
+        if (element.Length <= 3)
+        {
+            summ +=1;
+        }
+    }
+    var NewArray = new string[summ];
+    int index = 0;
+    foreach (var element in ArrayMethod)
+    {
+        if (element.Length <= 3)
+        {
+            NewArray[index] = element;
+            index++;
+        }
+    }
+    return NewArray;
+}
+
+
+void PrintArray(string[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        Write($"{inArray[i]}");
+        Write(i < inArray.Length - 1 ? ", " : "");
+    }
+    Write("]");
+}
